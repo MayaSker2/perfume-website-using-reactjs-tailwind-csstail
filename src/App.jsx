afadee8913ed4498d57eba62c43f5b0ce696c5ba
MayaSker2 <mayasker8@gmail.com>
2025-03-04 20:58:12 +0300

    second committ

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import React,{useEffect} from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css"; 
+import Navbar from './Component/Navbar/Navbar';
+// import Home from './Component/Home/Home';
+// import Services from './Component/Services/Services';
+// import AppStore from './Component/AppStore/AppStore';
+// import Testimonial from './Component/Testimonial/Testimonial';
+// import Footer from './Component/Footer/Footer';
+// import Banner from './Component/Banner/Banner';
 
-import './App.css'
-import Navbar from './Component/Navbar/Navbar'
-
-function App() {
-  
-
+const App = () => {
+  useEffect(() =>{
+    AOS.init({
+      offset:100,
+      duration:700,
+      easing:"ease-in",
+      delay:100,
+    });
+  });
   return (
-    <>
+    <div className='overflow-x-hidden'>
       <Navbar/>
-    </>
+      {/* <Home/>
+      <Services/>
+      <Banner/>
+      <AppStore/>
+      <Testimonial/>
+      <Footer/>  */}
+    </div>
   )
 }
-
+ 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans:["Poppins", "sana-serif"],
+        cursive:["Pacifico"," serif"],
+      },
+      colors:{
+        primary:"#831843",
+        secondary:"#be123c",
+        brandDark:"#270C03",
+      },
+      container:{
+        center: true,
+        padding:{
+          DEFAULT:"1rem",
+          sm:"2rem",
+          // lg:"4rem",
+          // xl:"5rem",
+          // sm:"2rem",
+        },
+      },
+    },
   },
   plugins: [],
 }
