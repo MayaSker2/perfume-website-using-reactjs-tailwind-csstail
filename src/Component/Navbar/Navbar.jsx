3d2a0398b138c6aabfe7775f6c3e2a2239c35e87
MayaSker2 <mayasker8@gmail.com>
2025-03-04 20:48:23 +0300

    first commit

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import mm from "../../assets/cat.jpg";
+const Navbar = () => {
+  return (
+    <div>
+      <img src={mm} alt="" className='w-20' />
+      <p className='bg-black text-red-300'>hhhhh</p>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default Navbar
